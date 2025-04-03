b4a198d29f2ec3c45ba6b5ada24eba59977fafe7
karrola <karolinahadzinska@gmail.com>
2025-04-03 21:23:35 +0200

    data_base_1

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'website'
+    app.config['SQLAlCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
+    import models 
+
     return app
+
