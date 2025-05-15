8f24105d0f9fcccfab9925faa22522a02c08562b
karrola <karolinahadzinska@gmail.com>
2025-05-15 14:09:48 +0200

    added requirements.txt

--- website/__init__.py
+++ website/__init__.py
@@ -4,27 +4,46 @@ from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate
 import os
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
+mail = Mail()
+
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'abc'
+
+    # podstawowa konfiguracja
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+    # konfiguracja Flask-Mail
+    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    
+    # inicjalizacja rozszerzeń
     db.init_app(app)
+    mail.init_app(app)
     migrate = Migrate(app, db)
 
+    # blueprints
     from .views import views
     from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # modele
     from .models import User, List
-
     create_database(app)
 
+    # login manager
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
